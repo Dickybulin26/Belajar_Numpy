5c88912070df39ac362438a4bda2186bfb819f04
Dickybulin26 <93518546+Dickybulin26@users.noreply.github.com>
2023-01-28 17:39:30 +0700

    Update main.py

--- 08-Advanced Array Creation/main.py
+++ 08-Advanced Array Creation/main.py
@@ -1,2 +1,34 @@
 import numpy as np
 
+#* membuat matrix dengan tipe data tertentu
+a = np.array(([1,2,3],[3,4,5]), dtype=float)
+
+print(a)
+#* membuat matrix denganmenggunakan function
+def kuadrat(baris,kolom):
+  return kolom**2
+
+def jumlah(baris,kolom):
+  return (kolom + baris)
+
+b = np.fromfunction(kuadrat, (1,10) , dtype=int)
+c = np.fromfunction(jumlah, (4,4), dtype=float)
+print(b)
+print(c)
+
+#* membuat matrix dengan menggunakan iterasi
+iterable = (x*2 for x in range(5))
+d = np.fromiter(iterable, dtype=int)
+print(d)
+
+#* multitype array
+dtipe = [('nama',"S255"), ("tinggi", int)]
+
+data = [
+  ("ucup",150),
+  ("otong",160),
+  ("mario",180)
+  ]
+
+e = np.array(data, dtype = dtipe)
+print(e[0])
